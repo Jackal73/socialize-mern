7751ca1d6583c41d187fb9030ba651bea0483d38
Shawn Kebel <fxrender@outlook.com>
2024-08-21 21:16:06 -0500

    :sparkles::children_crossing: Update userSlice to `use default user data` if `localStorage is empty`

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { user } from "../assets/data";
 
 const initialState = {
-  user: JSON.parse(window?.localStorage.getItem("user")) ?? {},
+  user: JSON.parse(window?.localStorage.getItem("user")) ?? user,
   edit: false,
 };
 
@@ -22,7 +23,6 @@ const userSlice = createSlice({
     },
   },
 });
-
 export default userSlice.reducer;
 
 export function UserLogin(user) {
