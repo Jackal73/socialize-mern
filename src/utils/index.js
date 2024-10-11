0cefdd523133be102aea203b78847c3195f35f06
Shawn Kebel <fxrender@outlook.com>
2024-10-10 22:23:30 -0500

    :sparkles::card_file_box: Add `file upload` utility function -> `Cloudinary`

--- src/utils/index.js
+++ src/utils/index.js
@@ -27,3 +27,18 @@ export const apiRequest = async ({ url, token, data, method }) => {
   }
 };
 
+export const handleFileUpload = async (uploadFile) => {
+  const formData = new FormData();
+  formData.append("file", uploadFile);
+  formData.append("upload_preset", "socialize");
+
+  try {
+    const response = await axios.post(
+      `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_ID}/image/upload/`,
+      formData
+    );
+    return response.data.secure_url;
+  } catch (error) {
+    console.log(error);
+  }
+};
