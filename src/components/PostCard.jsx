3349e35bdb6180570a00ec013a28f92dfe33b364
Shawn Kebel <fxrender@outlook.com>
2024-10-19 08:31:59 -0500

    :sparkles::children_crossing: Add `ReplyCard` component to display `replies to comments`

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { NoProfile } from "../assets";
+import moment from "moment";
+import { BiComment, BiLike, BiSolidLike } from "react-icons/bi";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import { useForm } from "react-hook-form";
+import TextInput from "./TextInput";
+import CustomButton from "./CustomButton";
+import Loading from "./Loading";
+import { apiRequest } from "../utils";
+
+const getPostComments = async (id) => {
+  try {
+    const res = await apiRequest({
+      url: "/posts/comments/" + id,
+      method: "GET",
+    });
+
+    return res?.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const ReplyCard = ({ reply, user, handleLike }) => {
+  return (
+    <div className="w-full py-3">
+      <div className="flex gap-3 items-center mb-1">
+        <Link to={"/profile/" + reply?.userId?._id}>
+          <img
+            src={reply?.userId?.profileUrl ?? NoProfile}
+            alt={reply?.userId?.firstName}
+            className="w-8 h-8 rounded-full object-cover"
+          />
+        </Link>
+        <div className="">
+          <Link to={"/profile/" + reply?.userId?._id}>
+            <p className="font-medium text-base text-ascent-1">
+              {reply?.userId?.firstName} {reply?.userId?.lastName}
+            </p>
+          </Link>
+          <span className="text-ascent-2 text-sm">
+            {moment(reply?.created_At ?? "2024-05-25").fromNow()}
+          </span>
+        </div>
+      </div>
+      <div className="ml-12">
+        <p className="text-ascent-2">{reply?.comment}</p>
+        <div className="mt-2 flex gap-6">
+          <p
+            className="flex gap-2 items-center text-base text-ascent-2 cursor-pointer"
+            onClick={handleLike}
+          >
+            {reply?.likes?.includes(user?._id) ? (
+              <BiSolidLike size={20} color="blue" />
+            ) : (
+              <BiLike size={20} />
+            )}
+            {reply?.likes?.length} Likes
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
