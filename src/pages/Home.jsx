332199b3478f215c704e09560819e741d31ca605
Shawn Kebel <fxrender@outlook.com>
2024-10-19 08:50:54 -0500

    :construction::sparkles: Add `FriendsCard` component to display `user friends` and implement `friend request functionality`

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   CustomButton,
   Loading,
+  PostCard,
   TextInput,
   TopBar,
 } from "../components";
+import FriendsCard from "../components/FriendsCard";
 import { Link } from "react-router-dom";
 import { NoProfile } from "../assets";
 import { BsFiletypeGif, BsPersonFillAdd } from "react-icons/bs";
@@ -85,11 +87,69 @@ const Home = () => {
     await deletePost(id, user.token);
     await fetchPost();
   };
+  const fetchFriendRequests = async () => {
+    try {
+      const res = await apiRequest({
+        url: "/users/get-friend-request",
+        token: user?.token,
+        method: "POST",
+      });
+      setFriendRequest(res?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchSuggestedFriends = async () => {
+    try {
+      const res = await apiRequest({
+        url: "/users/suggested-friends",
+        token: user?.token,
+        method: "POST",
+      });
+      setSuggestedFriends(res?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleFriendRequest = async (id) => {
+    try {
+      const res = await sendFriendRequest(user.token, id);
+      await fetchSuggestedFriends();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const acceptFriendRequest = async (id, status) => {
+    try {
+      const res = await apiRequest({
+        url: "/users/accept-request",
+        token: user?.token,
+        method: "POST",
+        data: { rid: id, status },
+      });
+      setFriendRequest(res?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const getUser = async () => {
     const res = await getUserInfo(user?.token);
     const newData = { token: user?.token, ...res };
     dispatch(UserLogin(newData));
   };
+
+  useEffect(() => {
+    setLoading(true);
+    getUser();
+    fetchPost();
+    fetchFriendRequests();
+    fetchSuggestedFriends();
+  }, []);
+
   return (
     <>
       <div className="w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor h-screen overflow-hidden">
@@ -97,11 +157,13 @@ const Home = () => {
         <div className="w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full">
           {/* LEFT */}
           <div className="hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6 overflow-y-auto">
+            <FriendsCard friends={user?.friends} />
           </div>
 
           {/* CENTER */}
           <div className="flex-1 h-full px-2 flex flex-col gap-6 overflow-y-auto">
             <form
+              onSubmit={handleSubmit(handlePostSubmit)}
               className="bg-primary px-4 rounded-xl"
             >
               <div className="w-full flex items-center gap-4 pt-4 pb-6">
@@ -222,14 +284,83 @@ const Home = () => {
                 {/* <span className="">{friendRequest?.length}</span> */}
               </div>
               <div className="w-full flex flex-col gap-4 pt-4">
+                {friendRequest?.map(({ _id, requestFrom: from }) => (
+                  <div key={_id} className="flex items-center justify-between">
+                    <Link
+                      to={"/profile/" + from._id}
+                      className="w-full flex gap-4 items-center cursor-pointer"
+                    >
+                      <img
+                        src={from?.profileUrl ?? NoProfile}
+                        alt={from?.firstName}
+                        className="w-10 h-10 object-cover rounded-full"
+                      />
+                      <div className="flex-1">
+                        <p className="text-base font-medium text-ascent-1">
+                          {from?.firstName} {from?.lastName}
+                        </p>
+                        <span className="text-sm text-ascent-2">
+                          {from?.profession ?? "No Profession"}
+                        </span>
+                      </div>
+                    </Link>
+                    <div className="flex gap-1">
+                      <CustomButton
+                        onClick={() => acceptFriendRequest(_id, "Accepted")}
+                        title="Accept"
+                        containerStyles="bg-[#0444a4] text-xs text-white px-1.5 py-1 rounded-full"
+                      />
+                      <CustomButton
+                        title="Deny"
+                        onClick={() => acceptFriendRequest(_id, "Denied")}
+                        containerStyles="border border-[#666] text-xs text-ascent-1 px-1.5 py-1 rounded-full"
+                      />
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
             {/* SUGGESTED FRIENDS */}
             <div className="w-full bg-primary shadow-sm rounded-xl px-5 py-5">
               <div className="pb-2 flex items-center justify-between text-lg text-ascent-1 border-b border-[#66666645]">
                 <span className="">Friend Suggestions</span>
+                {/* <span className="">{friendRequest?.length}</span> */}
               </div>
               <div className="w-full flex flex-col gap-4 pt-4">
+                {suggestedFriends?.map((friend) => (
+                  <div
+                    key={friend._id}
+                    className="flex items-center justify-between"
+                  >
+                    <Link
+                      to={"/profile/" + friend?._id}
+                      key={friend?._id}
+                      className="w-full flex gap-4 items-center cursor-pointer"
+                    >
+                      <img
+                        src={friend?.profileUrl ?? NoProfile}
+                        alt={friend?.firstName}
+                        className="w-10 h-10 object-cover rounded-full"
+                      />
+                      <div className="flex-1">
+                        <p className="text-base font-medium text-ascent-1">
+                          {friend?.firstName} {friend?.lastName}
+                        </p>
+                        <span className="text-sm text-ascent-2">
+                          {friend?.profession ?? "No Profession"}
+                        </span>
+                      </div>
+                    </Link>
+                    <div className="flex gap-1">
+                      <button
+                        className="bg-[#0444a430] text-sm text-white p-1 rounded"
+                        onClick={() => handleFriendRequest(friend?._id)}
+                      >
+                        <BsPersonFillAdd size={20} className="text-[#0f52b6]" />
+                      </button>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
